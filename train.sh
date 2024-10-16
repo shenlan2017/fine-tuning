d3715da0e9ad547c110f53a8b3f352c9c3aee5f7
Magic Yang <105222039+pzc163@users.noreply.github.com>
2024-10-16 20:08:30 +0800

    update train_args_file

--- train.sh
+++ train.sh
@@ -5,7 +5,6 @@ unset HIP_PATH
 unset ROCM_VERSION
 CUDA_VISIBLE_DEVICES=0,1
 
-# 3090 或者 4090 需启用 如果是A系列卡可以取消
 export NCCL_P2P_DISABLE=1
 export NCCL_IB_DISABLE=1
 
@@ -14,3 +13,4 @@ echo
 # 启动DeepSpeed训练
 deepspeed --include localhost:0,1 train.py \
           --train_args_file  $train_args_file
+
--- train_tools.sh
+++ train_tools.sh
@@ -9,7 +9,6 @@ unset ROCM_VERSION
 CUDA_VISIBLE_DEVICES=0,1
 train_args_file=$1
 
-# 3090 或者 4090 需启用 如果是A系列卡可以取消
 export NCCL_P2P_DISABLE=1
 export NCCL_IB_DISABLE=1
 
